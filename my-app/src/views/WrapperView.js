470c2418f91b3169b9ce31b2772b14ca8253b8fa
yonatanbensimon <106496877+yonatanbensimon@users.noreply.github.com>
2024-03-23 16:12:02 +0000

    Implemented Wrapper View

---
+++ my-app/src/views/WrapperView.js
@@ -0,0 +1,12 @@
+import HomeView from "./HomeView/HomeView";
+
+function WrapperView() {
+    return (
+      <div>
+        < HomeView />
+      </div>
+    );
+  }
+  
+  export default WrapperView;
+  
