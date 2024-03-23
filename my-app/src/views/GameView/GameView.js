93193e6dfd0cfb4960cc0005f16ad6f93aa40e8f
yonatanbensimon <106496877+yonatanbensimon@users.noreply.github.com>
2024-03-23 20:40:58 +0000

    Added Button Choice

--- my-app/src/views/GameView/GameView.js
+++ my-app/src/views/GameView/GameView.js
@@ -1,8 +1,15 @@
+import ButtonChoice from "../../components/ButtonChoice";
 import TextBox from "../../components/TextBox";
+import ButtonContainer from "../../components/ButtonChoiceController";
+import doctor from "../../assets/doctor.png"
 
 function GameView() {
     return (
         <div>
+            <ButtonContainer>
+                <ButtonChoice src={doctor} onClick={NaN}/>
+                <ButtonChoice src={doctor} onClick={NaN}/>
+            </ButtonContainer>
            <TextBox message={"This is a test"}/>
         </div>
     );
